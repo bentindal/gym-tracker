d666b6a7e030bf99f13984d994916a23b863b07f
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-06-02 20:23:40 +0100

    Changed to port 12345

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "require_all"
 
-set :port, 1234
+set :port, 12345
 
 enable :sessions
 set :session_secret, "$g]Rd2M/WbJ`~~<GZWdH@Fm'ESk2_gckCtLJJkySYG"
