e7a6e531add02bbe39afff67f81548339723c6f8
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-06-04 01:28:36 +0100

    Port 400

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "require_all"
 
-set :port, 433
+set :port, 400
 
 enable :sessions
 set :session_secret, "$g]Rd2M/WbJ`~~<GZWdH@Fm'ESk2_gckCtLJJkySYG"
