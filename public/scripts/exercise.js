68ec0fe18343b0fbf79233c3b9688473885d962d
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-07-20 10:29:51 +0100

    Prevented incorrcet rest times showing

--- public/scripts/exercise.js
+++ public/scripts/exercise.js
@@ -4,9 +4,17 @@ function time() {
     var d = new Date();
     var s = d.getSeconds();
     var m = d.getMinutes();
-    var h = d.getHours(); 
+    var h = d.getHours();
     theTime = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
     clock.innerText = theTime
+}
+
+function getCurrentDate(){
+    let d = new Date();
+    const day = d.getDate();
+    const month = d.getMonth() + 1;
+    const year = d.getFullYear(); 
+    return ("0" + day).substr(-2) + "/" + ("0" + month).substr(-2) + "/" + year;
 }
 
 function getCurrentTime(){
@@ -25,7 +33,15 @@ function getSecondsSince(time) {
     var total2 = parseInt(time2[0])*60*60 + parseInt(time2[1])*60 + parseInt(parseInt(time2[2]))
     var difference = total2 - total1
     var result = 0
-    if (difference >= 3600) {
+    console.log(lastDate)
+    console.log(getCurrentDate())
+    if (lastDate != getCurrentDate()){
+        result = ""
+    }
+    else if (difference > (60*60*24)){
+        result = ""
+    }
+    else if (difference >= 3600) {
         result = "last set " + Math.floor(difference / 3600)+ " hours " + (Math.floor(difference / 60) - Math.floor(difference / 3600)*60) + " minutes " + difference % 60 + " seconds ago"
     }
     else if (difference >= 60) {
