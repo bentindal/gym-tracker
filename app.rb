6f49cbb8649e45d71f136c5c34f3c9f8ea49b55e
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-08-10 19:24:05 +0100

    Fixed bug with editing sets

--- app.rb
+++ app.rb
@@ -115,7 +115,7 @@ post "/edit-set" do
   t = params[:Time]
   d = params[:Date]
   editSet(i, r, w, t, d)
-  redirect "/workouts/exercise?id="+params[:id]
+  redirect "/"
 end
 
 get "/delete-set" do
