33be45b9500c4de07a2b4f8366351ca302f41658
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-05-24 18:56:22 +0100

    Profile reworked

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -17,15 +17,18 @@ class FeedController < ApplicationController
       @exercises = Exercise.where(user_id: @user.id)
       @dates.each do |date|
         @list = []
+        @displayedExercises = []
         @exercises.each do |exercise|
           @listofsets = @sets.where(exercise_id: exercise.id).where(created_at: date.beginning_of_day..date.end_of_day)
           if @listofsets.first != nil
             @list.push([exercise, @listofsets])
+            @displayedExercises.push(exercise.group)
           end
         end
+        @displayedExercises.uniq!
         # Only if the user has a workout on the date, add it to the feed
         if @list.first[1].first != nil
-          @feed.push([@list.first[1].first.created_at, @user, @list])
+          @feed.push([@list.first[1].first.created_at, @user, @list, @displayedExercises])
         end
       end
     end
