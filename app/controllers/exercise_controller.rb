382add9a86326e6ac9736c63fae9812ac20a2f43
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-05-25 00:40:25 +0100

    Modified exercise to be able to edit

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -2,15 +2,41 @@ class ExerciseController < ApplicationController
   def index
     @exercises = Exercise.where(user_id: current_user.id)
   end
+
   def view
-    @exercise = Exercise.find(params[:id])
+    redirect_to "/exercise/#{params[:id]}"
   end
   
   def new
-    @exercise = Exercise.new(exercise_params)
-    @exercise.save
-    redirect_to exercises_url
+    @exercise = Exercise.new
   end
+  
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+  
+    if @exercise.save
+      redirect_to "/", notice: "Exercise created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+
+  def edit
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def update
+    @exercise = Exercise.find(params[:id])
+    if @exercise.update(exercise_params)
+      redirect_to "/", notice: "Exercise updated successfully"
+    else
+      puts @exercise.errors.full_messages # Output error messages to the console
+      redirect_to edit_exercise_path(@exercise), notice: "Error updating exercise"
+    end
+  end
+  
 
   def destroy
     @exercise = Exercise.find(params[:id])
@@ -19,9 +45,8 @@ class ExerciseController < ApplicationController
   end
 
   private
-    # Only allow a list of trusted parameters through.
     def exercise_params
-      params.permit(:user_id, :name, :unit, :group)
+      params.require(:exercise).permit(:user_id, :name, :unit, :group)
     end
   
-  end
+end
