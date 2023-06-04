9f11cb0c0acccdbca9ea5cb2b0a0f7b40c0ee780
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-06-05 00:00:04 +0100

    Set default timezone to London

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module GymTrackerRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "London"
   end
 end
