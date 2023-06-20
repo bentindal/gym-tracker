bac70b6d5a7c745f68cb39bd38833e77353323e7
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-06-20 17:11:57 +0100

    Removed username from db

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,9 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
     private
   
         def sign_up_params
-            params.require(:user).permit(:first_name, :last_name, :username, :email, :isPublic, :password, :name)
+            params.require(:user).permit(:first_name, :last_name, :email, :isPublic, :password, :password_confirmation, :name)
+            
         end
         def account_update_params
-            params.require(:user).permit(:first_name, :last_name, :username, :email, :isPublic, :password, :name, :current_password)
+            params.require(:user).permit(:first_name, :last_name, :email, :isPublic, :password, :name, :current_password)
         end
   end
