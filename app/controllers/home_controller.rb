cca3b3021c3c3adf6044ed305a7307876754b90a
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-07-06 22:38:47 +0100

    Updated home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @page_title = "Home"
-    @page_description = "Digital Gym Workout Tracker. Track your workouts & follow your friends to see what they are up to."
+    @page_description = "GymTracker is a simple easy-to-use workout tracker allowing you to track your workouts, follow your friends, and see what they're up to."
   end
 end
