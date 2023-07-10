72efeee999efb30ca17e31766f85d47d0c5bc386
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-07-10 14:24:33 +0100

    Updated page title for home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @page_title = "Home"
+    @page_title = "GymTracker - Free Online Gym Tracker & Workout Log"
     @page_description = "GymTracker is a simple easy-to-use workout tracker allowing you to track your workouts, follow your friends, and see what they're up to."
   end
 end
