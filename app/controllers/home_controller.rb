e7cf0d90fbac081662c6623d955881173748adbb
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-07-19 21:16:20 +0100

    Updated meta title tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @page_title = "GymTracker - Free Online Gym Tracker & Workout Log"
+    @page_title = "Free Online Gym Tracker & Workout Log"
     @page_description = "GymTracker is a simple easy-to-use gym tracker & workout log allowing you to track your workouts, follow your friends, and see what they're up to."
   end
 end
