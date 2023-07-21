5899385accfd72034431c36eeba9bcd438b8e82d
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-07-21 23:38:19 +0100

    Fixed bug on analysis page where no sets=error

--- app/controllers/exercise_controller.rb
+++ app/controllers/exercise_controller.rb
@@ -13,12 +13,18 @@ class ExerciseController < ApplicationController
 
   def view
     @exercise = Exercise.find(params[:id])
-    if params[:from] == nil
-      params[:from] = @exercise.workouts.order(:created_at).first.created_at.strftime("%d/%m/%Y")
+    if @exercise.workouts.count > 0
+      if params[:from] == nil
+        params[:from] = @exercise.workouts.order(:created_at).first.created_at.strftime("%d/%m/%Y")
+      end
+      if params[:to] == nil
+        params[:to] = @exercise.workouts.order(:created_at).last.created_at.strftime("%d/%m/%Y")
+      end
+    else
+      params[:from] = DateTime.now.strftime("%d/%m/%Y")
+      params[:to] = DateTime.now.strftime("%d/%m/%Y")
     end
-    if params[:to] == nil
-      params[:to] = @exercise.workouts.order(:created_at).last.created_at.strftime("%d/%m/%Y")
-    end
+    
   end
   
   def new
