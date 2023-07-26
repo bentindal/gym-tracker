a7d602c56d7da3176715c0fd492e7eb4f8303ac3
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-07-26 15:26:52 +0100

    Signed in users redirect to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # If users signed in redirect to dashboard
+    if user_signed_in?
+      redirect_to dashboard_path
+    end
     @page_title = "Free Online Gym Tracker & Workout Log"
     @page_description = "GymTracker is a simple easy-to-use gym tracker & workout log allowing you to track your workouts, follow your friends, and see what they're up to."
   end
