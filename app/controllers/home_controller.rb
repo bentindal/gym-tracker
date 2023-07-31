f0106d4e85fd1cec8a59a461c241e1df323632aa
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-07-31 17:01:18 +0100

    Updated home title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     if user_signed_in?
       redirect_to dashboard_path
     end
-    @page_title = "Free Online Gym Tracker & Workout Log"
-    @page_description = "GymTracker is a simple easy-to-use gym tracker & workout log allowing you to track your workouts, follow your friends, and see what they're up to."
+    @page_title = "Free Online Gym Tracker & Workout Diary"
+    @page_description = "GymTracker is a simple easy-to-use gym tracker & workout diary allowing you to track your workouts, follow your friends, and see what they're up to."
   end
 end
