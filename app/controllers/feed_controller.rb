6f4cdda1a6232bade63685340372b7d6ce7072d2
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-08-02 13:24:16 +0100

    Improved feed_controller speed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,6 +3,8 @@ class FeedController < ApplicationController
     @page_title = "Feed"
     @page_description = "View all your & your friends workouts on GymTracker"
 
+    from = 0
+    to = 10
     userList = [current_user]
     if params[:filter] != 'you'
       list_of_ids = Friend.where(user: current_user.id, confirmed: true).pluck(:follows)
@@ -12,11 +14,19 @@ class FeedController < ApplicationController
     end
 
     @feed = []
-    userList.each do |user|
-      all_workouts = Workout.where(user_id: user.id)
-      all_workouts.each do |workout|
-        @feed.push(workout.feed)
-      end
+    if params[:from] != nil && params[:to] != nil
+      from = params[:from].to_i
+      to = params[:to].to_i
+      all_workouts = Workout.where(user_id: userList).order(:created_at).reverse_order[from...to]
+    else
+      all_workouts = Workout.where(user_id: userList).order(:created_at).reverse_order
+    end
+
+    @workouts_processed = 0
+
+    all_workouts.each do |workout|
+      @feed.push(workout.feed)
+      @workouts_processed += 1
     end
     
     @feed = @feed.sort_by { |a| a[0] }.reverse
