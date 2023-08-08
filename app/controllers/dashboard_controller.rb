81438a8ab1375403888156b24912b7e937d77256
Benjamin Tindal <btindal1@sheffield.ac.uk>
2023-08-08 22:06:42 +0100

    Updated efficiency of feed loadtime

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,21 +2,12 @@ class DashboardController < ApplicationController
   def view
     @page_title = "Dashboard"
     @page_description = "View your dashboard on GymTracker"
+    
     # Feed
-    userList = [current_user]
-    if params[:filter] != 'you'
-      list_of_ids = Friend.where(user: current_user.id, confirmed: true).pluck(:follows)
-      list_of_ids.each do |id|
-        userList.push(User.find(id))
-      end
-    end
-
     @feed = []
-    userList.each do |user|
-      all_workouts = Workout.where(user_id: user.id)
-      all_workouts.each do |workout|
-        @feed.push(workout.feed)
-      end
+    all_workouts = Workout.where(user_id: current_user.id)
+    all_workouts.each do |workout|
+      @feed.push(workout.feed)
     end
     
     @feed = @feed.sort_by { |a| a[0] }.reverse[0..2]
