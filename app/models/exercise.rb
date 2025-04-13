2df97fca0ba51b41a778b452a999a30aa278ae1f
Benjamin Tindal <ben.tindal@mayden.co.uk>
2025-04-13 16:36:03 +0100

    feat: Enhance date handling in Exercise model and view for improved user experience
    - Updated `graph_total_volume` and `graph_orm` methods to set default date ranges if not provided.
    - Modified date input fields in the exercise view to default to one month ago and current date when no parameters are given.
    - Rounded the increase calculation in the view for better precision in displayed results.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -31,6 +31,8 @@ class Exercise < ApplicationRecord
   end
 
   def graph_total_volume(from, to)
+    from = from.present? ? from : 1.month.ago.to_s
+    to = to.present? ? to : Time.current.to_s
     calculate_graph_data(from, to) do |sets|
       sets.sum do |set|
         set.weight.to_f * set.repetitions.to_i unless set.isWarmup || set.weight.nil? || set.repetitions.nil?
@@ -39,12 +41,14 @@ class Exercise < ApplicationRecord
   end
 
   def graph_orm(from, to)
+    from = from.present? ? from : 1.month.ago.to_s
+    to = to.present? ? to : Time.current.to_s
     calculate_graph_data(from, to) do |sets|
       sets.map do |set|
         unless set.isWarmup || set.weight.nil? || set.repetitions.nil?
-          set.weight.to_f * (1 + (set.repetitions.to_i / 30.0))
+          (set.weight.to_f * (1 + (set.repetitions.to_i / 30.0))).round(2)
         end
-      end.compact.max.to_f.round(2)
+      end.compact.max
     end
   end
 
