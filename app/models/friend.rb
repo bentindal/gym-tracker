9f0015e3277e79721b63847b859d06ff8462a90e
Benjamin Tindal <ben.tindal@mayden.co.uk>
2025-04-17 12:09:20 +0100

    feat: enhance friend management features by adding follow, unfollow, accept, and reject actions; refactor friendship loading methods for clarity and efficiency

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Friend < ApplicationRecord
+  belongs_to :follower, class_name: 'User', foreign_key: 'user'
+  belongs_to :followed, class_name: 'User', foreign_key: 'follows'
 end
