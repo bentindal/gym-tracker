d42c80c8761b0da63e8a0e2d0b528116e6e87878
Benjamin Tindal <ben.tindal@mayden.co.uk>
2025-04-17 12:18:58 +0100

    Revert "Revert "refactor: update authentication logic in ApplicationController to allow unauthenticated access to new and create actions""
    This reverts commit 3d4c6058eff2e747e2ebff24a3fbe3e8d937216b.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 # The ApplicationController serves as the base controller for all other controllers.
 # It includes common functionality such as user authentication and CSRF protection.
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :new, :create]
 
   protect_from_forgery with: :exception
 
