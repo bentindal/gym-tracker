8f5c86d40839ee70e55962e2a8dfe2c55a892598
Benjamin Tindal <ben.tindal@mayden.co.uk>
2025-04-18 10:15:33 +0100

    feat: add AllsetHelper module for date formatting and set display logic; refactor exercise group badge rendering in show view

---
+++ app/helpers/allset_helper.rb
@@ -0,0 +1,37 @@
+module AllsetHelper
+  def format_date(date)
+    if date > Time.now.beginning_of_day - 10.minutes
+      content_tag(:div, "0s ago", "data-rest-timer-target": "display", style: "min-height: 1.2em")
+    elsif date > Time.now.beginning_of_day - 1.day
+      "Today"
+    elsif date > Time.now.beginning_of_day - 2.day
+      "Yesterday"
+    elsif date > Time.now.beginning_of_day - 6.days
+      date.strftime("%A")
+    else
+      date.strftime("%d %B, %Y")
+    end
+  end
+
+  def set_row_class(set)
+    classes = []
+    classes << "text-danger" if set.isFailure
+    classes << "text-info" if set.isDropset
+    classes << "small text-muted" if set.isWarmup
+    classes.join(" ")
+  end
+
+  def format_set_weight(set)
+    if set.weight == 0
+      "BW"
+    elsif set.weight.nil?
+      "?"
+    else
+      "#{set.weight}#{@exercise.unit}"
+    end
+  end
+
+  def format_set_repetitions(set)
+    set.repetitions.nil? ? "?" : set.repetitions
+  end
+end 
