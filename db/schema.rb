edcd3667e4f97eed3165cd39c74bf68187ec13ab
Benjamin Tindal <btindal1@sheffield.ac.uk>
2025-04-20 15:12:03 +0100

    chore: downgrade ActiveRecord schema version to 6.1 for compatibility

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_19_104508) do
+ActiveRecord::Schema[6.1].define(version: 2025_04_19_104508) do
   create_table "allsets", force: :cascade do |t|
     t.integer "exercise_id"
     t.integer "repetitions"
