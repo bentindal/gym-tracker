46b86e5630a007a3b35dd70cdc9cc0b3b5a41dd3
Benjamin Tindal <ben.tindal@mayden.co.uk>
2025-04-27 20:30:50 +0100

    feat: add location tracking in application controller and update workout view for navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@
 # It includes common functionality such as user authentication and CSRF protection.
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: %i[index new create]
+  before_action :set_location
   
   # Handle CSRF protection for GitHub Codespaces
   protect_from_forgery with: :exception, unless: :github_codespaces?
@@ -13,6 +14,22 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def set_location
+    referrer = request.referrer
+    return unless referrer
+
+    if referrer.include?('/feed')
+      @location = '/feed'
+    elsif referrer.include?('/dashboard')
+      @location = '/dashboard'
+    elsif referrer.include?('/users/view')
+      # Extract user ID from the referrer URL
+      if referrer =~ /\/users\/view\?id=(\d+)/
+        @location = "/users/view?id=#{$1}"
+      end
+    end
+  end
 
   def github_codespaces?
     ENV['CODESPACES'] == 'true' || request.base_url.include?('github.dev')
